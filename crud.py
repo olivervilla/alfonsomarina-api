a97598aa15ca4f2f19dc0a6280fe6af7fec8b71c
olivervilla <olivervilla3096@gmail.com>
2021-11-13 06:46:40 +0000

    all queries and nicer order

--- crud.py
+++ crud.py
@@ -11,11 +11,6 @@ LOG = logging.getLogger(__name__)
 # ---------------------------------------------
 #   TEAM MATE
 # ---------------------------------------------
-def get_teammate(db: Session, user_id: int):
-    table = schemas.Teammates.__table__
-    return db.execute(table.select().where(schemas.Teammates.id == user_id)).first()
-
-
 def create_teammate(db: Session, user: models.TeammateCreate):
     # TODO: hash password
     db_user = schemas.Teammates(**user.dict())
@@ -25,22 +20,34 @@ def create_teammate(db: Session, user: models.TeammateCreate):
     return db_user
 
 
+def read_teammate(db: Session, user_id: int):
+    table = schemas.Teammates.__table__
+    return db.execute(table.select().where(schemas.Teammates.id == user_id)).first()
+
+
+def read_teammate_all(db: Session):
+    return db.query(schemas.Teammates).all()
+
+
 # ---------------------------------------------
 #   DEPARTMENT
 # ---------------------------------------------
-def get_departament(db: Session, department_id: int):
-    table = schemas.Departments.__table__
-    return db.execute(table.select().where(schemas.Departments.id == department_id)).first()
-
-
-def get_departament_by_name(db: Session, departament_name: str):
-    table = schemas.Departments.__table__
-    return db.execute(table.select().where(schemas.Departments.name == departament_name)).first()
-
-
 def create_department(db: Session, department: models.DepartmentCreate):
     db_department = schemas.Departments(name=department.name)
     db.add(db_department)
     db.commit()
     db.refresh(db_department)
     return db_department
+
+def read_departament(db: Session, department_id: int):
+    table = schemas.Departments.__table__
+    return db.execute(table.select().where(schemas.Departments.id == department_id)).first()
+
+
+def read_department_all(db: Session):
+    return db.query(schemas.Departments).all()
+
+
+def read_departament_by_name(db: Session, departament_name: str):
+    table = schemas.Departments.__table__
+    return db.execute(table.select().where(schemas.Departments.name == departament_name)).first()
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, HTTPException, Depends
+from typing import List
 import crud, models, schemas
 import uvicorn
 
@@ -20,33 +21,49 @@ def get_db():
         db.close()
 
 
+# ---------------------------------------------
+#   TEAMMATE / USER
+# ---------------------------------------------
 @app.post("/users/", response_model=models.Teammate)
 async def create_user(user: models.TeammateCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_teammate(db, user_id=user.id)
+    db_user = crud.read_teammate(db, user_id=user.id)
     if db_user:
         raise HTTPException(status_code=400, detail="User code already registered")
     return crud.create_teammate(db, user)
 
 
+@app.get("/users/", response_model=List[models.Teammate])
+async def get_user_all(db: Session = Depends(get_db)):
+    return crud.read_teammate_all(db)
+
+
 @app.get("/users/{user_id}", response_model=models.Teammate)
-async def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_teammate(db, user_id=user_id)
+async def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.read_teammate(db, user_id=user_id)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
 
+# ---------------------------------------------
+#   DEPARTMENT
+# ---------------------------------------------
 @app.post("/departments/", response_model=models.Department)
 async def create_department(department: models.DepartmentCreate, db: Session = Depends(get_db)):
-    db_department = crud.get_departament_by_name(db, departament_name=department.name)
+    db_department = crud.read_departament_by_name(db, departament_name=department.name)
     if db_department:
         raise HTTPException(status_code=400, detail="Department already registered")
     return crud.create_department(db, department)
 
 
+@app.get("/departments/", response_model=List[models.Department])
+async def get_department_all(db: Session = Depends(get_db)):
+    return crud.read_department_all(db)
+
+
 @app.get("/departments/{department_id}", response_model=models.Department)
-async def read_user(department_id: int, db: Session = Depends(get_db)):
-    db_department = crud.get_departament(db, department_id=department_id)
+async def get_department(department_id: int, db: Session = Depends(get_db)):
+    db_department = crud.read_departament(db, department_id=department_id)
     if db_department is None:
         raise HTTPException(status_code=404, detail="Department not found")
     return db_department
